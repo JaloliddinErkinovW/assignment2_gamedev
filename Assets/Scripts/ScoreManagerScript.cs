b8c388ad3286c576f7f7641899c06b9425f51405
Jaloliddin Erkinov <132933556+JaloliddinErkinovW@users.noreply.github.com>
2024-09-28 23:26:56 +0500

    Add files via upload

---
+++ Assets/Scripts/ScoreManagerScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreManagerScript : MonoBehaviour {
+
+    public static int Score { get; set; }
+
+	// Use this for initialization
+	void Start () {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (previousScore != Score) //save perf from non needed calculations
+        { 
+            if(Score < 10)
+            {
+                //just draw units
+                Units.sprite = numberSprites[Score];
+            }
+            
+        }
+
+	}
+
+
+    int previousScore = -1;
+    public Sprite[] numberSprites;
+    public SpriteRenderer Units;
+}
